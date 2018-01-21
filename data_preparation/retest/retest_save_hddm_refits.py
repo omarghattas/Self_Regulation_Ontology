077d2959ef300e608aee57b9e267b7d19d5cba6f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-01-20 19:28:46 -0800

    Revert "Retest scripts"

--- data_preparation/retest/retest_save_hddm_refits.py
+++ data_preparation/retest/retest_save_hddm_refits.py
@@ -1,12 +1,20 @@
+import sys
+sys.path.append('/Users/zeynepenkavi/Dropbox/PoldrackLab/expfactory-analysis')
+sys.path.append('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/data_preparation')
 from os import path
 import pandas as pd
 
-data_dir = path.join('/oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data','Retest_01-17-2018/Local')
+try: 
+    release_date
+except NameError:
+    release_date = input('Enter release_ date:')
+    
+data_dir = data_dir=path.join('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/','Retest_'+release_date)
 
 #Read in DVs and valence
 label = 'hddm_refit'
-DV_df = pd.read_json(path.join(data_dir,'mturk_' + label + '_DV.json'))
-valence_df = pd.read_json(path.join(data_dir,'mturk_' + label + '_DV_valence.json'))
+DV_df = pd.read_json(path.join(data_dir,'Local/mturk_' + label + '_DV.json'))
+valence_df = pd.read_json(path.join(data_dir,'Local/mturk_' + label + '_DV_valence.json'))
 
 readme_lines = []
 
