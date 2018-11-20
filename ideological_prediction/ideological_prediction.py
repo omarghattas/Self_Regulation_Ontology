2aa9b6fe3b3c9092a796d3529fe6ba08e786bd2d
ian <ianeisenberg90@gmail.com>
2018-11-19 19:29:38 -0800

    moved and updated ideological prediction scripts

--- ideological_prediction/ideological_prediction.py
+++ ideological_prediction/ideological_prediction.py
@@ -1,5 +1,6 @@
 from fancyimpute import SoftImpute
 from itertools import product
+from os import path
 import pandas as pd
 
 from dimensional_structure.utils import hierarchical_cluster
@@ -7,20 +8,18 @@ from dimensional_structure.prediction_utils import run_prediction
 from dimensional_structure.prediction_plots import plot_prediction
 from selfregulation.utils.plot_utils import  save_figure
 from selfregulation.utils.result_utils import load_results
-from selfregulation.utils.utils import get_behav_data, get_recent_dataset
+from selfregulation.utils.utils import get_behav_data, get_recent_dataset, get_info
 
 dataset = get_recent_dataset()
 results = load_results(dataset)
-ideo_data = get_behav_data(dataset, file='ideology.csv').drop('WCST_Accuracy', axis=1)
-classifiers = ['lasso', 'ridge',  'svm', 'rf']
-
-
+ideo_data = get_behav_data(dataset, file='ideology.csv')
+results_dir = path.join(get_info('results_directory'), 'ideology_prediction')
+plot_dir = path.join(results_dir, 'Plots')
 
 # run prediction
 target_name = 'ideology'
 target = ideo_data+1E-10 # adding a small float causes values to be seen as continuous by "type_of_target"
-shuffle = False
-classifier = classifiers[1]
+shuffle_reps = 2
 
 # define predictors
 survey_scores = results['survey'].EFA.get_scores()
@@ -31,37 +30,66 @@ predictors = {'survey': survey_scores,
               'full_ontology': pd.concat([survey_scores, task_scores], axis=1)}
 # define targets
 targets = {'ideo_factors': ideo_data.filter(regex='Factor'),
-           'ideo_measures': ideo_data.drop(ideo_data.filter(regex='Factor').columns, axis=1)}
+           'ideo_orientations': ideo_data.drop(ideo_data.filter(regex='Factor|SECS').columns, axis=1),
+           'ideo_policies': ideo_data.filter(regex='SECS')}
 for key, target in targets.items():
     imputed = pd.DataFrame(SoftImpute().complete(target),
                             index=target.index,
                             columns=target.columns)
     targets[key] = imputed
 
+# do prediction with ontological factors
 predictions = {}
 shuffled_predictions = {}
+classifier = 'ridge'
 for predictor_key, scores in predictors.items():
     for target_key, target in targets.items():
         predictions[(predictor_key, target_key)] = \
                     run_prediction(scores, 
                                    target, 
-                                   '/home/ian/tmp',
+                                   results_dir,
                                    outfile='%s_%s_prediction' % (predictor_key, target_key), 
-                                   shuffle=shuffle,
+                                   shuffle=False,
                                    classifier=classifier, 
                                    verbose=True,
-                                   save=False,
+                                   save=True,
                                    binarize=False)['data']
         shuffled_predictions[(predictor_key, target_key)] = \
                             run_prediction(scores, 
                                            target, 
-                                           '/home/ian/tmp',
+                                           results_dir,
                                            outfile='%s_%s_prediction' % (predictor_key, target_key), 
-                                           shuffle=200,
+                                           shuffle=shuffle_reps,
                                            classifier=classifier, 
                                            verbose=True,
-                                           save=False,
+                                           save=True,
                                            binarize=False)['data']
+        
+# predictions with raw measures
+predictor_key = 'raw_measures'
+scores = get_behav_data()
+classifier = 'lasso'
+for target_key, target in targets.items():
+    predictions[(predictor_key, target_key)] = \
+                run_prediction(scores, 
+                               target, 
+                               results_dir,
+                               outfile='%s_%s_prediction' % (predictor_key, target_key), 
+                               shuffle=False,
+                               classifier=classifier, 
+                               verbose=True,
+                               save=True,
+                               binarize=False)['data']
+    shuffled_predictions[(predictor_key, target_key)] = \
+                        run_prediction(scores, 
+                                       target, 
+                                       results_dir,
+                                       outfile='%s_%s_prediction' % (predictor_key, target_key), 
+                                       shuffle=shuffle_reps,
+                                       classifier=classifier, 
+                                       verbose=True,
+                                       save=True,
+                                       binarize=False)['data']                           
 
 for target in targets.keys():
     imputed = targets[target]
