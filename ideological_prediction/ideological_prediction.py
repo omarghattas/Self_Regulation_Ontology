d8d3d57204378d2fd6590bdbc501213b64d54550
Ian <ianeisenberg90@gmail.com>
2018-12-05 10:27:07 -0800

    added rotation and argparse to ideological script

--- ideological_prediction/ideological_prediction.py
+++ ideological_prediction/ideological_prediction.py
@@ -4,17 +4,34 @@ def warn(*args, **kwargs):
 import warnings
 warnings.warn = warn
 
+# imports
+import argparse
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-classifier', default='ridge')
+parser.add_argument('-raw_classifier', default='lasso')
+parser.add_argument('-EFA_rotation', default='oblimin')
+parser.add_argument('-shuffle_repeats', type=int, default=1000)
+args = parser.parse_args()
+
 from fancyimpute import SoftImpute
 import json
 from os import makedirs, path
 import numpy as np
 import pandas as pd
 import pickle
-
 from dimensional_structure.prediction_utils import run_prediction
 from selfregulation.utils.result_utils import load_results
 from selfregulation.utils.utils import get_behav_data, get_recent_dataset, get_info
 
+
+# parse args
+classifier = args.classifier
+raw_classifier = args.raw_classifier
+shuffle_reps = args.shuffle_repeats
+EFA_rotation = args.EFA_rotation
+
+# load data
 dataset = get_recent_dataset()
 results = load_results(dataset)
 ideo_data = get_behav_data(dataset, file='ideology.csv')
@@ -22,16 +39,15 @@ results_dir = path.join(get_info('results_directory'), 'ideology_prediction')
 makedirs(results_dir, exist_ok=True)
 
 # run prediction
-target_name = 'ideology'
 shuffle_reps = 1000
 
 # define predictors
-survey_scores = results['survey'].EFA.get_scores()
-task_scores = results['task'].EFA.get_scores()
-predictors = {'survey': survey_scores,
-              'task': task_scores,
+survey_scores = results['survey'].EFA.get_scores(rotate=EFA_rotation)
+task_scores = results['task'].EFA.get_scores(rotate=EFA_rotation)
+predictors = {'survey_%s' % EFA_rotation: survey_scores,
+              'task_%s' % EFA_rotation: task_scores,
               'demographics': results['task'].DA.get_scores(),
-              'full_ontology': pd.concat([survey_scores, task_scores], axis=1)}
+              'full_ontology_%s' % EFA_rotation: pd.concat([survey_scores, task_scores], axis=1)}
 # define targets
 targets = {'ideo_factors': ideo_data.filter(regex='Factor'),
            'ideo_orientations': ideo_data.drop(ideo_data.filter(regex='Factor|SECS').columns, axis=1).drop(['Conservatism','Intellectual Humility'], axis=1),
@@ -45,7 +61,6 @@ for key, target in targets.items():
 # do prediction with ontological factors
 predictions = {}
 shuffled_predictions = {}
-classifier = 'ridge'
 for predictor_key, scores in predictors.items():
     for target_key, target in targets.items():
         print('*'*79)
@@ -71,12 +86,11 @@ for predictor_key, scores in predictors.items():
                                            verbose=True,
                                            save=True,
                                            binarize=False)['data']
-        
+
 # predictions with raw measures
 predictor_key = 'raw_measures'
 DV_scores = get_behav_data(file='meaningful_variables_imputed.csv')
 predictors['raw_measures'] = DV_scores
-classifier = 'lasso'
 for target_key, target in targets.items():
     print('Running Prediction: raw measures predicting %s' % target_key)
     predictions[(predictor_key, target_key)] = \
@@ -85,7 +99,7 @@ for target_key, target in targets.items():
                                results_dir,
                                outfile='%s_%s_prediction' % (predictor_key, target_key), 
                                shuffle=False,
-                               classifier=classifier, 
+                               classifier=raw_classifier, 
                                verbose=True,
                                save=True,
                                binarize=False)['data']
@@ -95,7 +109,7 @@ for target_key, target in targets.items():
                                        results_dir,
                                        outfile='%s_%s_prediction' % (predictor_key, target_key), 
                                        shuffle=shuffle_reps,
-                                       classifier=classifier, 
+                                       classifier=raw_classifier, 
                                        verbose=True,
                                        save=True,
                                        binarize=False)['data']                           
@@ -109,7 +123,8 @@ data = {'all_predictions': predictions,
                         
 # save all results
 pickle.dump(data, 
-            open(path.join(results_dir, 'ideo_predictions.pkl'), 'wb'))
+            open(path.join(results_dir, 
+                           'ideo_predictions_%s.pkl' % EFA_rotation), 'wb'))
 
 # save results in easier-to-use format
 simplified = {}
@@ -140,8 +155,11 @@ for t,vals in targets.items():
     simplified['Target_Cat'].update({'insample_' + c:t for c in vals.columns})
         
 simplified=pd.DataFrame(simplified)
-simplified.to_csv(path.join(results_dir, 'predictions_R2.csv'))
-json.dump(predictor_importances, open(path.join(results_dir, 'predictor_importances.json'), 'w'))
+simplified.to_csv(path.join(results_dir, '
+                            predictions_%s_R2.csv' % EFA_rotation))
+json.dump(predictor_importances, 
+          open(path.join(results_dir, 
+                         'predictor_importances_%s.json' % EFA_rotation), 'w'))
 
 
 
