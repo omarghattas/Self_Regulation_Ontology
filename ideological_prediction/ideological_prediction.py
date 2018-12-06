2450e0ad03335c020fb9eca46fa7abd484e80cd4
Ian <ianeisenberg90@gmail.com>
2018-12-05 23:57:23 -0800

    adding arguments to ideo prediction script

--- ideological_prediction/ideological_prediction.py
+++ ideological_prediction/ideological_prediction.py
@@ -8,12 +8,14 @@ warnings.warn = warn
 import argparse
 # parse arguments
 parser = argparse.ArgumentParser()
+parser.add_argument('-skip_factors', action='store_true')
+parser.add_argument('-skip_raw', action='store_true')
 parser.add_argument('-classifier', default='ridge')
 parser.add_argument('-raw_classifier', default='lasso')
 parser.add_argument('-EFA_rotation', default='oblimin')
 parser.add_argument('-shuffle_repeats', type=int, default=1000)
 args = parser.parse_args()
-
+    
 from fancyimpute import SoftImpute
 import json
 from os import makedirs, path
@@ -26,6 +28,8 @@ from selfregulation.utils.utils import get_behav_data, get_recent_dataset, get_i
 
 
 # parse args
+run_factors = not args.skip_factors
+run_raw = not args.skip_raw
 classifier = args.classifier
 raw_classifier = args.raw_classifier
 shuffle_reps = args.shuffle_repeats
@@ -38,16 +42,7 @@ ideo_data = get_behav_data(dataset, file='ideology.csv')
 results_dir = path.join(get_info('results_directory'), 'ideology_prediction')
 makedirs(results_dir, exist_ok=True)
 
-# run prediction
-shuffle_reps = 1000
 
-# define predictors
-survey_scores = results['survey'].EFA.get_scores(rotate=EFA_rotation)
-task_scores = results['task'].EFA.get_scores(rotate=EFA_rotation)
-predictors = {'survey_%s' % EFA_rotation: survey_scores,
-              'task_%s' % EFA_rotation: task_scores,
-              'demographics': results['task'].DA.get_scores(),
-              'full_ontology_%s' % EFA_rotation: pd.concat([survey_scores, task_scores], axis=1)}
 # define targets
 targets = {'ideo_factors': ideo_data.filter(regex='Factor'),
            'ideo_orientations': ideo_data.drop(ideo_data.filter(regex='Factor|SECS').columns, axis=1).drop(['Conservatism','Intellectual Humility'], axis=1),
@@ -58,84 +53,109 @@ for key, target in targets.items():
                             columns=target.columns)
     targets[key] = imputed+1E-5
 
-# do prediction with ontological factors
+# ****************************************************************
+# run prediction
+# ****************************************************************
+predictors = {}
 predictions = {}
 shuffled_predictions = {}
-for predictor_key, scores in predictors.items():
+# do prediction with ontological factors
+if run_factors:
+    # define predictors
+    survey_scores = results['survey'].EFA.get_scores(rotate=EFA_rotation)
+    task_scores = results['task'].EFA.get_scores(rotate=EFA_rotation)
+    predictors.update({'survey_%s' % EFA_rotation: survey_scores,
+                      'task_%s' % EFA_rotation: task_scores,
+                      'demographics': results['task'].DA.get_scores(),
+                      'full_ontology_%s' % EFA_rotation: pd.concat([survey_scores, task_scores], axis=1)})
+    for predictor_key, scores in predictors.items():
+        for target_key, target in targets.items():
+            print('*'*79)
+            print('Running Prediction: %s predicting %s' % (predictor_key, target_key))
+            print('*'*79)
+            predictions[(predictor_key, target_key)] = \
+                        run_prediction(scores, 
+                                       target, 
+                                       results_dir,
+                                       outfile='%s_%s_prediction' % (predictor_key, target_key), 
+                                       shuffle=False,
+                                       classifier=classifier, 
+                                       verbose=True,
+                                       save=True,
+                                       binarize=False)['data']
+            print('\nRunning Shuffle Prediction\n')
+            shuffled_predictions[(predictor_key, target_key)] = \
+                                run_prediction(scores, 
+                                               target, 
+                                               results_dir,
+                                               outfile='%s_%s_prediction' % (predictor_key, target_key), 
+                                               shuffle=shuffle_reps,
+                                               classifier=classifier, 
+                                               verbose=True,
+                                               save=True,
+                                               binarize=False)['data']
+
+if run_raw:
+    # predictions with raw measures
+    predictor_key = 'raw_measures'
+    DV_scores = get_behav_data(file='meaningful_variables_imputed.csv')
+    predictors['raw_measures'] = DV_scores
     for target_key, target in targets.items():
         print('*'*79)
-        print('Running Prediction: %s predicting %s' % (predictor_key, target_key))
+        print('Running Prediction: raw measures predicting %s' % target_key)
         print('*'*79)
         predictions[(predictor_key, target_key)] = \
-                    run_prediction(scores, 
+                    run_prediction(DV_scores, 
                                    target, 
                                    results_dir,
                                    outfile='%s_%s_prediction' % (predictor_key, target_key), 
                                    shuffle=False,
-                                   classifier=classifier, 
+                                   classifier=raw_classifier, 
                                    verbose=True,
                                    save=True,
                                    binarize=False)['data']
+        print('\nRunning Shuffle Prediction\n')
         shuffled_predictions[(predictor_key, target_key)] = \
-                            run_prediction(scores, 
+                            run_prediction(DV_scores, 
                                            target, 
                                            results_dir,
                                            outfile='%s_%s_prediction' % (predictor_key, target_key), 
                                            shuffle=shuffle_reps,
-                                           classifier=classifier, 
+                                           classifier=raw_classifier, 
                                            verbose=True,
                                            save=True,
-                                           binarize=False)['data']
-
-# predictions with raw measures
-predictor_key = 'raw_measures'
-DV_scores = get_behav_data(file='meaningful_variables_imputed.csv')
-predictors['raw_measures'] = DV_scores
-for target_key, target in targets.items():
-    print('Running Prediction: raw measures predicting %s' % target_key)
-    predictions[(predictor_key, target_key)] = \
-                run_prediction(DV_scores, 
-                               target, 
-                               results_dir,
-                               outfile='%s_%s_prediction' % (predictor_key, target_key), 
-                               shuffle=False,
-                               classifier=raw_classifier, 
-                               verbose=True,
-                               save=True,
-                               binarize=False)['data']
-    shuffled_predictions[(predictor_key, target_key)] = \
-                        run_prediction(DV_scores, 
-                                       target, 
-                                       results_dir,
-                                       outfile='%s_%s_prediction' % (predictor_key, target_key), 
-                                       shuffle=shuffle_reps,
-                                       classifier=raw_classifier, 
-                                       verbose=True,
-                                       save=True,
-                                       binarize=False)['data']                           
-
-# data
-data = {'all_predictions': predictions,
-        'all_shuffled_predictions': shuffled_predictions,
-        'predictors': predictors,
-        'targets': targets}
-                        
+                                           binarize=False)['data']                           
+  
+# ****************************************************************
+# save
+# ****************************************************************
+filename = path.join(results_dir, 'ideo_predictions.pkl')
+if path.exists(filename):
+    data = pickle.load(open(filename, 'rb'))
+else:
+    data = {'all_predictions': {},
+            'all_shuffled_predictions': {},
+            'predictors': {},
+            'targets': {}}
+# update data
+data['all_predictions'].update(predictions)
+data['all_shuffled_predictions'].update(shuffled_predictions)
+data['predictors'].update(predictors)
+data['targets'].update(targets)
                         
 # save all results
-pickle.dump(data, 
-            open(path.join(results_dir, 
-                           'ideo_predictions_%s.pkl' % EFA_rotation), 'wb'))
+pickle.dump(data,  open(filename, 'wb'))
 
 # save results in easier-to-use format
 simplified = {}
 simplified_importances = {}
 predictor_importances = {}
-for p in predictors.keys():
+for p in data['predictors'].keys():
     simplified[p] = {}
     simplified
     predictor_importances[p] = {}
-    for t in targets.keys():
-        tmp = predictions[(p,t)]
+    for t in data['targets'].keys():
+        tmp = data['all_predictions'][(p,t)]
         tmp_scores = {'CV_'+k:tmp[k]['scores_cv'][0]['R2'] for k in tmp.keys()}
         tmp_scores.update({'insample_'+k:tmp[k]['scores_insample'][0]['R2'] for k in tmp.keys()})
         simplified[p].update(tmp_scores)
@@ -150,16 +170,16 @@ for p in predictors.keys():
                                                  key = lambda x: abs(x[1]), 
                                                  reverse=True)
 simplified['Target_Cat'] = {}
-for t,vals in targets.items():
+for t,vals in data['targets'].items():
     simplified['Target_Cat'].update({'CV_' + c:t for c in vals.columns})
     simplified['Target_Cat'].update({'insample_' + c:t for c in vals.columns})
         
 simplified=pd.DataFrame(simplified)
 simplified.to_csv(path.join(results_dir, 
-                            'predictions_%s_R2.csv' % EFA_rotation))
+                            'predictions_R2.csv'))
 json.dump(predictor_importances, 
           open(path.join(results_dir, 
-                         'predictor_importances_%s.json' % EFA_rotation), 'w'))
+                         'predictor_importances.json'), 'w'))
 
 
 
