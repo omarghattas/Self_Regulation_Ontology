d36893d9ce9793488586df54c0093968109f6a82
ian <ianeisenberg90@gmail.com>
2018-12-10 16:28:01 -0800

    added new importance plot

--- ideological_prediction/ideological_plots.py
+++ ideological_prediction/ideological_plots.py
@@ -4,7 +4,8 @@ import pandas as pd
 import pickle
 from ideological_prediction.plot_utils import (plot_outcome_ontological_similarity,
                                                plot_prediction, plot_prediction_scatter,
-                                               polar_plots,
+                                               importance_bar_plots,
+                                               importance_polar_plots,
                                                plot_predictors_comparison)
 from selfregulation.utils.utils import  get_info
 
@@ -14,6 +15,7 @@ makedirs(plot_dir, exist_ok=True)
 
 
 # load predictions
+rotate='oblimin'
 ext = 'pdf'
 data = pickle.load(open(path.join(results_dir, 
                                 'ideo_predictions.pkl'), 'rb'))
@@ -25,31 +27,42 @@ targets = data['targets']
 for target in targets.keys():
     predictions = odict()
     shuffled_predictions = odict()
-    for predictor_key in ['demographics', 'task', 'survey', 'full_ontology', 'raw_measures']:
-        predictions[predictor_key] = all_predictions[(predictor_key, target)]
-        shuffled_predictions[predictor_key] = all_shuffled_predictions[(predictor_key, target)]
-        # plot scatter plot
-        plot_prediction_scatter(predictions[predictor_key],
-            predictors[predictor_key],
-            targets[target],
-            size=15,
-            filename=path.join(plot_dir, '%s_%s_scatter.%s' % (predictor_key, target, ext)))
-        # plot ontological similarity
-        if predictor_key != 'raw_measures':
-            plot_outcome_ontological_similarity(predictions[predictor_key],
-                            size=15,
-                            filename=path.join(plot_dir, '%s_%s_similarity.%s' % (predictor_key, target, ext)))
+    for predictor_key in ['demographics', 'task_%s' % rotate, 
+                          'survey_%s' % rotate, 'full_ontology_%s' % rotate, 
+                          'raw_measures']:
+        if (predictor_key, target) in all_predictions.keys():
+            predictions[predictor_key] = all_predictions[(predictor_key, target)]
+            shuffled_predictions[predictor_key] = all_shuffled_predictions[(predictor_key, target)]
+            # plot scatter plot
+            plot_prediction_scatter(predictions[predictor_key],
+                predictors[predictor_key],
+                targets[target],
+                size=15,
+                filename=path.join(plot_dir, '%s_%s_scatter.%s' % (predictor_key, target, ext)))
+            # plot importance bars
+            importance_bar_plots(predictions[predictor_key], target_order=targets[target].columns,
+                     size=10,
+                     filename=path.join(plot_dir, '%s_%s_importance_bar.%s' % (predictor_key, target,ext)))
+        
+            # plot ontological similarity
+            if predictor_key != 'raw_measures':
+                plot_outcome_ontological_similarity(predictions[predictor_key],
+                                size=15,
+                                filename=path.join(plot_dir, '%s_%s_similarity.%s' % (predictor_key, target, ext)))
+            else:
+                print("Key couldn't be found! %s, %s" % (predictor_key, target))
     plot_prediction(predictions, shuffled_predictions, 
                     target_order=targets[target].columns,
                     filename=path.join(plot_dir, '%s_bar.%s' % (target, ext)))
     # drop some unneeded predictions
     for key in ['demographics', 'raw_measures']:
-        del predictions[key]
+        if key in predictions.keys():
+            del predictions[key]
     if target == 'ideo_factors':
-        polar_plots(predictions, target_order=targets[target].columns,
+        importance_polar_plots(predictions, target_order=targets[target].columns,
                     size=10,
-                    filename=path.join(plot_dir, '%s_polar_plots.%s' % (target,ext)))
-        
+                    filename=path.join(plot_dir, '%s_importance_polar.%s' % (target,ext)))
+
 # compare performance as distribution
 simplified = pd.read_csv(path.join(results_dir, 'predictions_R2.csv'), 
                          index_col=0)
